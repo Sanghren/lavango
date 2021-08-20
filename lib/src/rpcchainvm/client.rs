2024e308c2504d57597f3e228c1fa265288ddad3
tbrunain <dev@bouh.io>
2021-08-20 15:22:24 +0200

    feat: Setup rpcchainvm client and server (#1)

--- lib/src/rpcchainvm/client.rs
+++ lib/src/rpcchainvm/client.rs
@@ -2,20 +2,12 @@ use hello_world::greeter_client::GreeterClient;
 use hello_world::HelloRequest;
 
 pub mod hello_world {
-    tonic::include_proto!("helloworld");
+    tonic::include_proto!("vm.proto");
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = GreeterClient::connect("http://[::1]:50051").await?;
-
-    let request = tonic::Request::new(HelloRequest {
-        name: "Tonic".into(),
-    });
-
-    let response = client.say_hello(request).await?;
-
-    println!("RESPONSE={:?}", response);
 
     Ok(())
 }
